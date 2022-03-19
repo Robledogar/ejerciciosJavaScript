463e1e5a7daaaf4279353ed07a25b5769ceae26e
Robledogar <45006680+Robledogar@users.noreply.github.com>
2022-03-19 11:54:18 +0100

    Add files via upload

---
+++ 25 beginner JavaScript proyects/pass the message/script.js
@@ -0,0 +1,8 @@
+const messageInput = document.getElementById("message-input");
+
+
+
+function getMessage() {
+	document.getElementById("message-output").innerHTML = messageInput.value;
+	messageInput.value = " ";
+}
